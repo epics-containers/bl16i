c9005d645da150d2c8b8175aea9aa57e383a6865
Giles Knap <giles.knap@diamond.ac.uk>
2023-10-04 21:28:19 +0100

    fix environment.sh

--- environment.sh
+++ environment.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# setup the environment for using the BLXXI beamline
-
-set -e
-
 echo "Loading IOC environment for BLXXI ..."
 
-if ! ec --version &> /dev/null; then
-    pip install epics-containers-cli
-fi
 
 # a mapping between genenric IOC repo roots and the related container registry
 export EC_REGISTRY_MAPPING='github.com=ghcr.io gitlab.diamond.ac.uk=gcr.io/diamond-privreg/controls/ioc'
@@ -16,21 +9,22 @@ export EC_K8S_NAMESPACE=blxxi
 export EC_EPICS_DOMAIN=blxxi
 export EC_GIT_ORG=https://github.com/epics-containers
 export EC_LOG_URL='https://graylog2.diamond.ac.uk/search?rangetype=relative&fields=message%2Csource&width=1489&highlightMessage=&relative=172800&q=pod_name%3A{ioc_name}*'
+# export EC_CONTAINER_CLI=podman (not specified so ec can detect best option)
 
-# ibek is not required outside of the generic IOC containers but is occasionally useful
-source <(ec --show-completion bash)
-if ibek --version &> /dev/null; then
-    source <(ibek --show-completion bash)
-fi
-
-# the following configures kubernetes inside DLS. But this script is used in CI
-# so skip this step if we are not on the DLS network
-if module --version &> /dev/null; then
+# the following configures kubernetes inside DLS.
+if module --version; then
     if module avail pollux > /dev/null; then
         module load pollux > /dev/null
-        # allow this to fail if the cluster is not available
-        if ! ec ps ; then echo "no cluster available"; fi
     fi
 fi
 
+# install the epics-containers-cli if (ec command) if we are in a venv
+if [[ -n "$VIRTUAL_ENV" ]] ; then
+    if ! ec --version &> /dev/null; then
+        pip install epics-containers-cli
+    fi
+else
+    echo "Please activate a virtualenv and re-run to install ec commandline tool"
+fi
 
+exec ${SHELL}
