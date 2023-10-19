287c9433719f3c5b87331c56fa7b21be358afe3b
Giles Knap <gilesknap@gmail.com>
2023-10-19 19:09:27 +0000

    update to latest from bl45p

--- environment.sh
+++ environment.sh
@@ -1,30 +1,50 @@
 #!/bin/bash
 
-echo "Loading IOC environment for BLXXI ..."
+# check we are sourced
+if [ "$0" = "$BASH_SOURCE" ]; then
+    echo "ERROR: Please source this script"
+    exit 1
+fi
 
+THIS_DIR=$(dirname ${0})
+
+echo "Loading IOC environment for BL45P ..."
 
 # a mapping between genenric IOC repo roots and the related container registry
 export EC_REGISTRY_MAPPING='github.com=ghcr.io gitlab.diamond.ac.uk=gcr.io/diamond-privreg/controls/ioc'
-export EC_K8S_NAMESPACE=blxxi
-export EC_EPICS_DOMAIN=blxxi
+# the namespace to use for kubernetes deployments
+export EC_K8S_NAMESPACE=bl45p
+# the git organisation used for beamline repositories
 export EC_GIT_ORG=https://github.com/epics-containers
+# the git repo for this beamline (or accelerator domain)
+export EC_DOMAIN_REPO=git@github.com:epics-containers/bl45p.git
+# declare your centralised log server Web UI
 export EC_LOG_URL='https://graylog2.diamond.ac.uk/search?rangetype=relative&fields=message%2Csource&width=1489&highlightMessage=&relative=172800&q=pod_name%3A{ioc_name}*'
-# export EC_CONTAINER_CLI=podman (not specified so ec can detect best option)
+# enforce a specific container cli - defaults to whatever is available
+# export EC_CONTAINER_CLI=podman
+# enable debug output in all 'ec' commands
+# export EC_DEBUG=1
 
 # the following configures kubernetes inside DLS.
-if module --version; then
+# replace this with however your cluster is configured - essenially needs to
+# ~/.kube/config
+if module --version &> /dev/null; then
     if module avail pollux > /dev/null; then
+        module unload pollux > /dev/null
         module load pollux > /dev/null
     fi
 fi
 
-# install the epics-containers-cli if (ec command) if we are in a venv
-if [[ -n "$VIRTUAL_ENV" ]] ; then
-    if ! ec --version &> /dev/null; then
-        pip install epics-containers-cli
+# check if epics-containers-cli (ec command) is installed and install if not
+if ! ec --version &> /dev/null; then
+    # must be in a venv and this is the reliable check
+    if python3 -c 'import sys; sys.exit(0 if sys.base_prefix==sys.prefix else 1)'; then
+        echo "ERROR: Please activate a virtualenv and re-run"
+        return
+    elif ! ec --version &> /dev/null; then
+        pip install -r ${THIS_DIR}/requirements.txt
     fi
-else
-    echo "Please activate a virtualenv and re-run to install ec commandline tool"
 fi
 
-exec ${SHELL}
+# enable shell completion for ec commands
+source <(ec --show-completion ${SHELL})
--- opi/blxxi-ea-ioc-01-gui.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-$(dirname ${BASH_SOURCE[0]})/launcher.sh blxxi-ea-ioc-01 PCO.edl ${@}
